2f85d07b27d8b7c82be3680a3a501764ef97f0f5
hkder <samhskim1@gmail.com>
2023-01-21 22:13:37 -0500

    Added Base Django Backend

---
+++ backend/polls/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework import viewsets
+from .serializers import TodoSerializer
+from .models import Todo
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all()

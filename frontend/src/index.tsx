1ff42e406503d443b29c64d2f1a28b324d3d9057
Casey Szilagyi <caseyszilagyi@gmail.com>
2023-01-24 11:30:39 -0500

    folder name

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginPage from './components/loginPage';
+import LoginPage from './pages/loginPage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(

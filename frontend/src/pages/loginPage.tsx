3bcc62d15f0fceb62e290601685ed208de725114
Casey Szilagyi <caseyszilagyi@gmail.com>
2023-01-27 16:23:59 -0500

    some scaffolding

--- frontend/src/pages/loginPage.tsx
+++ frontend/src/pages/loginPage.tsx
@@ -1,29 +1,51 @@
-import React from 'react';
-import { FormEvent } from 'react';
+import React from "react";
+import { FormEvent } from "react";
+import axios from "axios";
 
 interface LoginState {
-  value: string
+  value: string;
 }
 
 class LoginPage extends React.Component<{}, LoginState> {
   constructor(props = {}) {
     super(props);
-    this.state = {value: ''};
+    this.state = { value: "" };
 
-    //this.handleChange = this.handleChange.bind(this);
-    //this.handleSubmit = this.handleSubmit.bind(this);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
 
   onChange = (event: FormEvent<HTMLInputElement>): void => {
-    this.setState({value: event.currentTarget.value});
-  }
+    this.setState({ value: event.currentTarget.value });
+  };
 
   onSubmit = (event: FormEvent<HTMLFormElement>): void => {
-    alert('A password was submitted: ' + this.state.value);
+    const loginPayload = {
+      email: "eve.holt@reqres.in",
+      password: "cityslicka",
+    };
 
-    
+    axios.post("API LINK", loginPayload).then((response) => {
+      //get token from response
+      const token = response.data.token;
+      console.log(token)
+
+      //set JWT token to local
+      localStorage.setItem("token", token);
+
+      //set token to axios common header
+      this.setAuthToken(token);
+    });
+
+    alert("A password was submitted: " + this.state.value);
 
     event.preventDefault();
+  };
+
+  setAuthToken(token: "string") {
+    if (token) {
+      axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    } else delete axios.defaults.headers.common["Authorization"];
   }
 
   render() {
@@ -31,8 +53,11 @@ class LoginPage extends React.Component<{}, LoginState> {
       <form onSubmit={(e: FormEvent<HTMLFormElement>) => this.onSubmit(e)}>
         <label>
           Password:
-          <input type="text" value={this.state.value} 
-            onChange={(e: FormEvent<HTMLInputElement>) => this.onChange(e)} />
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={(e: FormEvent<HTMLInputElement>) => this.onChange(e)}
+          />
         </label>
         <input type="submit" value="Submit" />
       </form>

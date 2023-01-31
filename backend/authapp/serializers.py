cd3ed917b515fd45a4251f06bef6c5e8576c08a8
Hosung Kim <hk196@duke.edu>
2023-01-30 22:55:25 -0500

    Update serializers.py
    Added PasswordSerializer for password change

--- backend/authapp/serializers.py
+++ backend/authapp/serializers.py
@@ -29,11 +29,11 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = (
+        fields = [
             'email',
             'username',
             'password',
-        )
+        ]
 
     def update(self, instance, validated_data):
         """Updates a User."""
@@ -50,6 +50,33 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         # After finishing this update, must explicitly save the model.
+        instance.save()
+
+        return instance
+
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password2 = serializers.CharField(write_only=True, required=True)
+    old_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('old_password', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
         instance.save()
 
         return instance

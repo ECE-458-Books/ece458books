f72f80bfe8b191ba3e5f490ba8af3130f5cd8820
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2023-01-30 20:25:25 -0500

    Refactor authentication code to be cleaned up and consolated to the useful operations. This includes removal of auth/token/ endpoint, which can now simply be retrieved from login

--- backend/authapp/serializers.py
+++ backend/authapp/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate
 from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
@@ -10,50 +11,15 @@ class RegistrationSerializer(serializers.ModelSerializer):
     # characters, and can not be read by the client.
     password = serializers.CharField(max_length=128, min_length=8, write_only=True)
 
-    # The client should not be able to send a token along with a registration
-    # request. Making `token` read-only handles that for us.
-    token = serializers.CharField(max_length=255, read_only=True)
-
     class Meta:
         model = User
         # List all of the fields that could possibly be included in a request
         # or response, including fields specified explicitly above.
-        fields = ['email', 'username', 'password', 'token']
+        fields = ['email', 'username', 'password']
 
     def create(self, validated_data):
         # Use the `create_user` method we wrote earlier to create a new user.
         return User.objects.create_user(**validated_data)
-
-
-class LoginSerializer(serializers.Serializer):
-    email = serializers.CharField(max_length=255)
-    username = serializers.CharField(max_length=255, read_only=True)
-    password = serializers.CharField(max_length=128, write_only=True)
-    token = serializers.CharField(max_length=255, read_only=True)
-
-    def validate(self, data):
-        '''
-        In this method we ensure the current LoginSerializer has instace of 'valid', meaning that for loggin in a user, validating that the user has provided an email and password that matches a user in the database.
-        '''
-        email = data.get('email', None)
-        password = data.get('password', None)
-
-        if email is None:
-            raise serializers.ValidationError('An email address is required to log in.')
-
-        if password is None:
-            raise serializers.ValidationError('A password is required to log in.')
-
-        user = authenticate(username=email, password=password)
-
-        if user is None:
-            raise serializers.ValidationError('No user found with this email and password.')
-
-        # Django has a flag on user that says if it has been banned or deactivated, which will almost never be the case, but doesn't hurt to check.
-        if not user.is_active:
-            raise serializers.ValidationError('This user has been deactivated.')
-
-        return {'email': user.email, 'username': user.username, 'token': user.token}
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -67,11 +33,7 @@ class UserSerializer(serializers.ModelSerializer):
             'email',
             'username',
             'password',
-            'token',
         )
-
-        # Same as doing 'read_only = True', just for 'token' we aren't specifying anything else about it like we do with 'password' above
-        read_only_fields = ('token',)
 
     def update(self, instance, validated_data):
         """Updates a User."""
@@ -84,7 +46,7 @@ class UserSerializer(serializers.ModelSerializer):
             setattr(instance, key, value)
 
         if password is not None:
-            # `.set_password()` handles all security-related tasks.
+            # `.set_password()` handles all security-related tasks. Implemented in super classes
             instance.set_password(password)
 
         # After finishing this update, must explicitly save the model.
--- backend/authapp/urls.py
+++ backend/authapp/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import RegistrationAPIView, LoginAPIView, UserRetrieveUpdateAPIView
 
 app_name = 'authapp'
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
     path('users/', RegistrationAPIView.as_view()),
     path('users/login/', LoginAPIView.as_view()),

4819b0d83de3dd184df652ff981f7da11ad66f40
hkder <samhskim1@gmail.com>
2023-01-31 05:52:10 -0500

    added book delete

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -5,6 +5,6 @@ app_name='books'
 
 urlpatterns = [
     path('isbns', ISBNSearchView.as_view()),
-    path('<str:isbn_13>', RetrieveUpdateDestroyBookAPIView.as_view()),
+    path('<isbn_13>', RetrieveUpdateDestroyBookAPIView.as_view()),
     path('', ListCreateBookAPIView.as_view()),
 ]

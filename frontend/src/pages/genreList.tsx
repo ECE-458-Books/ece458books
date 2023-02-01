06821287d90607f9925cb89b707ea79bc64401d7
Casey Szilagyi <caseyszilagyi@gmail.com>
2023-02-01 11:43:33 -0500

    file names

--- frontend/src/pages/genreList.tsx
+++ frontend/src/pages/genreList.tsx
@@ -1,18 +1,41 @@
 import React from "react";
+import Table, { TableColumn } from "../components/Table";
 
 interface GenreListState {
   value: string;
 }
 
-class GenreListPage extends React.Component<{}, GenreListState> {
+interface GenreRow {
+  genre: string;
+  numBooks: number;
+}
+
+// Currently being used for filtering in booklist, will have to change
+export const GENRE_DATA: GenreRow[] = [
+  {
+    genre: "blah",
+    numBooks: 5,
+  },
+];
+
+const COLUMNS: TableColumn[] = [
+  { field: "genre", header: "Genre", filterPlaceholder: "Search by Genre" },
+  {
+    field: "numBooks",
+    header: "Number of Books",
+    filterPlaceholder: "Search by Number of Books",
+  },
+];
+
+class GenreList extends React.Component<{}, GenreListState> {
   constructor(props = {}) {
     super(props);
     this.state = { value: "" };
   }
 
   render() {
-    return <h1>Dummy Genre List Page</h1>;
+    return <Table<GenreRow> columns={COLUMNS} data={GENRE_DATA} />;
   }
 }
 
-export default GenreListPage;
+export default GenreList;

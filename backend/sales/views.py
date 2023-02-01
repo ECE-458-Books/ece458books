f573bd3534b64e4e19fbbac7488158db1cf30714
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2023-01-31 20:51:24 -0500

    Add calculated fields for sales reconciliation

--- backend/sales/views.py
+++ backend/sales/views.py
@@ -16,4 +16,20 @@ class SalesReconciliationAPIView(ListCreateAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        response_data = self.add_calculated_fields(serializer)
+        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def add_calculated_fields(self, serializer):
+        total_revenue = 0
+        books = set()
+        num_books = 0
+        for sale in serializer.data['sales']:
+            sale_revenue = sale['quantity'] * sale['unit_retail_price']
+            total_revenue += sale_revenue
+            books.add(sale['book'])
+            num_books += sale['quantity']
+        response_data = serializer.data
+        response_data['total_revenue'] = total_revenue
+        response_data['num_unique_books'] = len(books)
+        response_data['num_books'] = num_books
+        return response_data

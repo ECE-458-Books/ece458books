1d61d24a682da3ef16145fdb5b98185c322b09ea
Casey Szilagyi <caseyszilagyi@gmail.com>
2023-02-05 12:03:14 -0500

    Refactor book add

--- frontend/src/pages/add/BookAdd.tsx
+++ frontend/src/pages/add/BookAdd.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 import { InputTextarea } from "primereact/inputtextarea";
 import { Button } from "primereact/button";
 import { DataTable } from "primereact/datatable";
@@ -98,13 +98,11 @@ const COLUMNS: TableColumn[] = [
   },
 ];
 
-class BookAdd extends React.Component<{}, BookAddState> {
-  constructor(props = {}) {
-    super(props);
-    this.state = { inputBoxText: "", data: DATA };
-  }
+export default function BookAdd() {
+  const [textBox, setTextBox] = useState("");
+  const [books, setBooks] = useState(DATA);
 
-  onCellEditComplete = (e: {
+  const onCellEditComplete = (e: {
     rowData: any;
     newValue: any;
     field: string;
@@ -132,7 +130,7 @@ class BookAdd extends React.Component<{}, BookAddState> {
     }
   };
 
-  cellEditor = (options: ColumnEditorOptions) => {
+  const cellEditor = (options: ColumnEditorOptions) => {
     switch (options.field) {
       case "pubYear":
         return numberEditor(options);
@@ -145,71 +143,68 @@ class BookAdd extends React.Component<{}, BookAddState> {
     }
   };
 
-  onSubmit = (): void => {
-    alert("A form was submitted: \n" + this.state.inputBoxText);
+  const onSubmit = (): void => {
+    console.log(textBox);
   };
 
-  onCompleteSubmit = (): void => {
+  const onCompleteSubmit = (): void => {
     alert("Form Data" + JSON.stringify(DATA));
   };
 
   //Two Forms exist in order for the seperate submission of two seperate types of data.
   //First one is the submission of ISBNS that need to be added
   //Second one is the submission of the added books and their modified fields
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit}>
-          <label htmlFor="addbook">Add Books (ISBN'S)</label>
-          <InputTextarea
-            id="addbook"
-            name="addbook"
-            value={this.state.inputBoxText}
-            onChange={(e: FormEvent<HTMLTextAreaElement>) =>
-              this.setState({ inputBoxText: e.currentTarget.value })
-            }
-            rows={5}
-            cols={30}
-          />
-          <Button
-            id="addbooksubmission"
-            name="addbooksubmission"
-            label="Submit"
-            type="submit"
-          />
-        </form>
-        <label htmlFor="addbookcompletion">Finish Book Addition</label>
-        <form onSubmit={this.onCompleteSubmit}>
-          <DataTable
-            value={DATA}
-            editMode="cell"
-            className="editable-cells-table"
-            responsiveLayout="scroll"
-          >
-            {COLUMNS.map(({ field, header }) => {
-              return (
-                <Column
-                  key={field}
-                  field={field}
-                  header={header}
-                  style={{ width: "25%" }}
-                  body={field === "retailPrice" && priceBodyTemplate}
-                  editor={(options) => this.cellEditor(options)}
-                  onCellEditComplete={this.onCellEditComplete}
-                />
-              );
-            })}
-          </DataTable>
-          <Button
-            id="confirmbooks"
-            name="confirmbooks"
-            label="Confirm Addition"
-            type="submit"
-          />
-        </form>
-      </div>
-    );
-  }
-}
 
-export default BookAdd;
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="addbook">Add Books (ISBN'S)</label>
+        <InputTextarea
+          id="addbook"
+          name="addbook"
+          value={textBox}
+          onChange={(e: FormEvent<HTMLTextAreaElement>) =>
+            setTextBox(e.currentTarget.value)
+          }
+          rows={5}
+          cols={30}
+        />
+        <Button
+          id="addbooksubmission"
+          name="addbooksubmission"
+          label="Submit"
+          type="submit"
+        />
+      </form>
+      <label htmlFor="addbookcompletion">Finish Book Addition</label>
+      <form onSubmit={onCompleteSubmit}>
+        <DataTable
+          value={DATA}
+          editMode="cell"
+          className="editable-cells-table"
+          responsiveLayout="scroll"
+        >
+          {COLUMNS.map(({ field, header }) => {
+            return (
+              <Column
+                key={field}
+                field={field}
+                header={header}
+                style={{ width: "25%" }}
+                body={field === "retailPrice" && priceBodyTemplate}
+                editor={(options) => cellEditor(options)}
+                onCellEditComplete={onCellEditComplete}
+              />
+            );
+          })}
+        </DataTable>
+        <Button
+          id="confirmbooks"
+          name="confirmbooks"
+          label="Confirm Addition"
+          type="submit"
+        />
+      </form>
+    </div>
+  );
+}

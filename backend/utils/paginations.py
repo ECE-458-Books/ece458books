737e9bc6412b6034808e1be2a6a80c18360e3d3f
Hosung Kim <hk196@duke.edu>
2023-02-07 14:03:45 -0500

    [BugFix] Bugfix change pagination next (#129)

---
+++ backend/utils/paginations.py
@@ -0,0 +1,55 @@
+from rest_framework.pagination import PageNumberPagination 
+from rest_framework.utils.urls import remove_query_param, replace_query_param
+
+class HTTPSNoPortPagination(PageNumberPagination):
+    """Custom Pagination Class that overrides the django PageNumberPagination
+       
+       The default get_next_link, get_previous_link returns the url with http and port number added
+
+       This class is changing the header to HTTPS and remove explicit port numbering
+    """
+    page_size = 10
+    page_size_query_param = 'page_size'
+
+    def get_next_link(self):
+        """Override get_next_link to support HTTPS and remove explicit port numbering
+
+        Returns:
+            next_link: "https://${server_name}/${request_uri}
+
+        """
+        if not self.page.has_next():
+            return None
+        url = self.request.build_absolute_uri()
+        reformat_url = self.reformat_url(url)
+        page_number = self.page.next_page_number()
+        return replace_query_param(reformat_url, self.page_query_param, page_number)
+    
+    def get_previous_link(self):
+        """Override get_previous_link to support HTTPS and remove explicit port numbering
+
+        Returns:
+            previous_link: "https://${server_name}/${request_uri}
+
+        """
+        if not self.page.has_previous():
+            return None
+        url = self.request.build_absolute_uri()
+        reformat_url = self.reformat_url(url)
+        page_number = self.page.previous_page_number()
+        if page_number == 1:
+            return remove_query_param(reformat_url, self.page_query_param)
+        return replace_query_param(reformat_url, self.page_query_param, page_number)
+    
+    def reformat_url(self, url):
+        reformat_url = url.replace("http://", "https://")
+        port = self.get_port_number(reformat_url)
+
+        reformat_url = reformat_url.replace(f":{port}", "")
+
+        return reformat_url
+    
+    def get_port_number(self, url):
+        if(len(url.split(":"))<2):
+            return None
+        return url.split(":")[2].split("/")[0]

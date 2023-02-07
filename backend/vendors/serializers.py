d695a38c6fb5a4cca4eae743a028f994d2a2d8ca
Hosung Kim <hk196@duke.edu>
2023-02-07 18:23:04 -0500

    [BugFix] Added Vendor Delete iff No PO Related & Changed Status Error to 409 (#141)

--- backend/vendors/serializers.py
+++ backend/vendors/serializers.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 
 from .models import Vendor
 
+from purchase_orders.models import PurchaseOrder
+
 class VendorSerializer(serializers.ModelSerializer):
-    # Uncomment when purchase_order app is made
-    # purchase_orders = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    num_purchase_orders = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Vendor
-        fields = '__all__'
+        fields = ['id', 'name', 'num_purchase_orders']
--- backend/vendors/views.py
+++ backend/vendors/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Count
+from django.db.models import Count, OuterRef, Subquery, Func
 
 from rest_framework import filters, status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
@@ -8,6 +8,8 @@ from rest_framework.response import Response
 from .serializers import VendorSerializer
 from .models import Vendor
 from .paginations import VendorPagination
+
+from purchase_orders.models import PurchaseOrder
 
 class ListCreateVendorAPIView(ListCreateAPIView):
     serializer_class = VendorSerializer
@@ -23,6 +25,25 @@ class ListCreateVendorAPIView(ListCreateAPIView):
             return None
         else:
             return super().paginate_queryset(queryset)
+    
+    def get_queryset(self):
+        default_query_set = Vendor.objects.all()
+
+        default_query_set = default_query_set.annotate(
+            num_purchase_orders=Subquery(
+                PurchaseOrder.objects.filter(
+                    vendor=OuterRef('id')
+                ).values_list(
+                    Func(
+                        'id',
+                        function='COUNT',
+                    ),
+                )
+            )
+        )
+        
+        return default_query_set
+
 
 class RetrieveUpdateDestroyVendorAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = VendorSerializer
@@ -31,14 +52,19 @@ class RetrieveUpdateDestroyVendorAPIView(RetrieveUpdateDestroyAPIView):
     lookup_url_kwarg = 'id'
 
     def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        
-        # Uncomment when purchase order is linked
+        # Check if the request url is valid
+        try:
+            instance = self.get_object()
+        except Exception as e:
+            return Response({"error": f"{e}"}, status=status.HTTP_204_NO_CONTENT)
+
         # Check to see if the Vendor has no purchase orders associated
-        # associated_purchase_order_cnt = len(instance.purchase_order_set.all())
-        # if(associated_purchase_order_cnt != 0):
-        #     error_msg = {"error": f"{associated_purchase_order_cnt} purchase order(s) associated with vendor:{instance.name}"}
-        #     return Response(error_msg, status=status.HTTP_400_BAD_REQUEST)
+        queryset = PurchaseOrder.objects.filter(vendor_id=instance.id)
+
+        associated_purchase_order_cnt = len(queryset)
+        if(associated_purchase_order_cnt != 0):
+            error_msg = {"error": f"{associated_purchase_order_cnt} purchase order(s) associated with vendor:{instance.name}"}
+            return Response(error_msg, status=status.HTTP_409_CONFLICT)
 
         self.perform_destroy(instance)
         return Response({"destroy": "success"}, status=status.HTTP_204_NO_CONTENT)

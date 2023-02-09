e716c98fefd355d5a54bc48c8596359d73c02a72
hac21 <hac21@duke.edu>
2023-02-08 19:22:56 -0500

    finished formatting sales report

--- frontend/src/pages/list/SalesReport.tsx
+++ frontend/src/pages/list/SalesReport.tsx
@@ -41,7 +41,7 @@ const COLUMNS_TOTAL: TableColumnSales[] = [
 ];
 
 const COLUMNS_DAILY: TableColumnSales[] = [
-  { field: "date", header: "Date" },
+  { field: "date", header: "Date (YYYY-MM-DD)" },
   { field: "revenue", header: "Revenue" },
   { field: "cost", header: "Cost" },
   { field: "profit", header: "Profit" },
@@ -57,7 +57,7 @@ const COLUMNS_TOP_BOOKS: TableColumnSales[] = [
 ];
 
 export default function SalesReport() {
-  const [totalsData, setTotalsData] = useState<salesReportTotalRow>();
+  const [totalsData, setTotalsData] = useState<salesReportTotalRow[]>([]);
   const [dailyData, setDailyData] = useState<salesReportDailyRow[]>([]);
   const [topBooksData, setTopBooksData] = useState<salesReportTopBooksRow[]>(
     []
@@ -67,13 +67,42 @@ export default function SalesReport() {
     useState(false);
 
   const onAPIResponse = (response: GetSalesReportResp) => {
-    setTotalsData({
-      revenue: response.revenue,
-      cost: response.cost,
-      profit: response.profit,
-    });
+    setTotalsData([
+      {
+        revenue: response.revenue,
+        cost: response.cost,
+        profit: response.profit,
+      },
+    ]);
     setDailyData(response.daily_summary);
     setTopBooksData(response.top_books);
+  };
+
+  const formatCurrency = (value: any) => {
+    return value.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
+  const priceBodyTemplateRevenue = (rowData: any) => {
+    return formatCurrency(rowData.revenue);
+  };
+  const priceBodyTemplateCost = (rowData: any) => {
+    return formatCurrency(rowData.cost);
+  };
+  const priceBodyTemplateProfit = (rowData: any) => {
+    return formatCurrency(rowData.profit);
+  };
+
+  const priceBodyTemplateBookRevenue = (rowData: any) => {
+    return formatCurrency(rowData.book_revenue);
+  };
+  const priceBodyTemplateBookCost = (rowData: any) => {
+    return formatCurrency(rowData.total_cost_most_recent);
+  };
+  const priceBodyTemplateBookProfit = (rowData: any) => {
+    return formatCurrency(rowData.book_profit);
   };
 
   const onSubmit = (): void => {
@@ -96,14 +125,14 @@ export default function SalesReport() {
   };
 
   return (
-    <div className="card flex justify-content-center">
-      <div className="col-11">
+    <div className="grid flex justify-content-center">
+      <div className="col-12">
         <div className="py-2">
           <h1 className="p-component p-text-secondary text-5xl text-center text-900 color: var(--surface-800);">
             Sales Report
           </h1>
           <form onSubmit={onSubmit}>
-            <div className="flex pb-2 flex-row justify-content-evenly card-container col-12">
+            <div className="flex pb-2 flex-row justify-content-evenly card-container">
               <div>
                 <label
                   htmlFor="date"
@@ -135,49 +164,85 @@ export default function SalesReport() {
               />
             </div>
           </form>
-
-          <div className="col-offset-3 col-6">
-            <DataTable value={totalsData} className="">
-              {COLUMNS_TOTAL.map(({ field, header }) => {
-                return (
-                  <Column
-                    key={field}
-                    field={field}
-                    header={header}
-                    style={{ width: "25%" }}
-                  />
-                );
-              })}
-            </DataTable>
-          </div>
-          <div className="col-offset-2 col-8">
-            <DataTable value={dailyData} className="">
-              {COLUMNS_DAILY.map(({ field, header }) => {
-                return (
-                  <Column
-                    key={field}
-                    field={field}
-                    header={header}
-                    style={{ width: "25%" }}
-                  />
-                );
-              })}
-            </DataTable>
-          </div>
-          <div className="col-offset-2 col-8">
-            <DataTable value={topBooksData} className="">
-              {COLUMNS_TOP_BOOKS.map(({ field, header }) => {
-                return (
-                  <Column
-                    key={field}
-                    field={field}
-                    header={header}
-                    style={{ width: "25%" }}
-                  />
-                );
-              })}
-            </DataTable>
-          </div>
+        </div>
+      </div>
+      <div className="pt-3 col-12 justify-content-center">
+        <h1 className="p-component p-text-secondary text-3xl text-center text-900 color: var(--surface-800);">
+          Totals Summary
+        </h1>
+      </div>
+      <div className="pt-3 col-12 justify-content-center">
+        <div className="justify-content-center col-offset-3 col-6">
+          <DataTable value={totalsData} className="">
+            {COLUMNS_TOTAL.map(({ field, header }) => {
+              return (
+                <Column
+                  key={field}
+                  field={field}
+                  header={header}
+                  style={{ width: "25%" }}
+                  body={
+                    (field === "revenue" && priceBodyTemplateRevenue) ||
+                    (field === "cost" && priceBodyTemplateCost) ||
+                    (field === "profit" && priceBodyTemplateProfit)
+                  }
+                />
+              );
+            })}
+          </DataTable>
+        </div>
+      </div>
+      <div className="pt-3 col-12 justify-content-center">
+        <h1 className="p-component p-text-secondary text-3xl text-center text-900 color: var(--surface-800);">
+          Daily Summary
+        </h1>
+      </div>
+      <div className="pt-3 col-12 justify-content-center">
+        <div className="col-offset-2 col-8">
+          <DataTable value={dailyData} className="">
+            {COLUMNS_DAILY.map(({ field, header }) => {
+              return (
+                <Column
+                  key={field}
+                  field={field}
+                  header={header}
+                  style={{ width: "25%" }}
+                  body={
+                    (field === "revenue" && priceBodyTemplateRevenue) ||
+                    (field === "cost" && priceBodyTemplateCost) ||
+                    (field === "profit" && priceBodyTemplateProfit)
+                  }
+                />
+              );
+            })}
+          </DataTable>
+        </div>
+        <div className="pt-3 col-12 justify-content-center align-content-center">
+          <h1 className="p-component p-text-secondary text-3xl text-center text-900 color: var(--surface-800);">
+            Top Book Total Summary
+          </h1>
+        </div>
+        <div className="col-offset-2 col-8">
+          <DataTable value={topBooksData} className="">
+            {COLUMNS_TOP_BOOKS.map(({ field, header }) => {
+              return (
+                <Column
+                  key={field}
+                  field={field}
+                  header={header}
+                  style={{ width: "25%" }}
+                  hidden={"book_id" === field}
+                  body={
+                    (field === "book_revenue" &&
+                      priceBodyTemplateBookRevenue) ||
+                    (field === "total_cost_most_recent" &&
+                      priceBodyTemplateBookCost) ||
+                    (field === "book_profit" && priceBodyTemplateBookProfit)
+                  }
+                />
+              );
+            })}
+          </DataTable>
         </div>
       </div>
     </div>

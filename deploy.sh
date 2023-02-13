0938be793dfa487199c938c5f7cb404302a77fc3
hkder <samhskim1@gmail.com>
2023-02-12 22:11:14 -0500

    almost working

--- deploy.sh
+++ deploy.sh
@@ -6,10 +6,12 @@
 # All rights reserved
 # ----------------------------------------------------------------------
 
+JENKINS_DEPLOY_HOME="/home/hk196/jenkins_auto_deploy/"
+
 echo "Copy FrondEnd Environment File"
-cp /home/hk196/.env.development frontend
+cp /home/hk196/.env.development "${JENKINS_DEPLOY_HOME}frontend"
 
 echo "Copy BackEnd Environment File"
-cp /home/hk196/.env backend
+cp /home/hk196/.env "${JENKINS_DEPLOY_HOME}backend"
 
 (cd /home/hk196/jenkins_auto_deploy && make local)

e3ac851d4b098f0edaa3c08659183795b88f265c
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2023-02-16 15:46:56 -0500

    Add purchase orders dummy endpoint for csv import to /api/v1/purchase_orders/csv/import

--- backend/purchase_orders/urls.py
+++ backend/purchase_orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListCreatePurchaseOrderAPIView, RetrieveUpdateDestroyPurchaseOrderAPIView
+from .views import ListCreatePurchaseOrderAPIView, RetrieveUpdateDestroyPurchaseOrderAPIView, CSVPurchasesAPIView
 
 app_name = 'purchase_orders'
 
 urlpatterns = [
     path('', ListCreatePurchaseOrderAPIView.as_view()),
-    path('/<id>', RetrieveUpdateDestroyPurchaseOrderAPIView.as_view())
+    path('/<id>', RetrieveUpdateDestroyPurchaseOrderAPIView.as_view()),
+    path('/csv/import', CSVPurchasesAPIView.as_view())
 ]

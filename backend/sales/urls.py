59ab0ffadf8ba19280efef72d1cceefd43be5cc3
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2023-02-17 21:40:06 -0500

    Update sales csv endpoint to /sales_reconciliation/csv/import

--- backend/sales/urls.py
+++ backend/sales/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('/sales_reconciliation', ListCreateSalesReconciliationAPIView.as_view()),
     path('/sales_reconciliation/<id>', RetrieveUpdateDestroySalesReconciliationAPIView.as_view()),
     path('/sales_report/start:<start_date>end:<end_date>', RetrieveSalesReportAPIView.as_view()),
-    path('/csv/import', CSVSaleAPIView.as_view()),
+    path('/sales_reconciliation/csv/import', CSVSaleAPIView.as_view()),
 ]

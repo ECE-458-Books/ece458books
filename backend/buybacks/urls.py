cc35c3d09981ac69c82ab7355d218397aad5c9c9
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2023-02-27 11:11:18 -0500

    implement buyback csv import

--- backend/buybacks/urls.py
+++ backend/buybacks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListCreateBuybackAPIView, RetrieveUpdateDestroyBuybackAPIView
+from .views import ListCreateBuybackAPIView, RetrieveUpdateDestroyBuybackAPIView, CSVBuybackAPIView
 
 app_name = 'buybacks'
 
 urlpatterns = [
     path('', ListCreateBuybackAPIView.as_view()),
-    path('/<id>', RetrieveUpdateDestroyBuybackAPIView.as_view())
+    path('/<id>', RetrieveUpdateDestroyBuybackAPIView.as_view()),
+    path('/csv/import', CSVBuybackAPIView.as_view()),
 ]

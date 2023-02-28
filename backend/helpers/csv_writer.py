014fa22786c10f55cdc8b25817336455360254b3
hkder <samhskim1@gmail.com>
2023-02-28 11:03:20 -0500

    refactor

--- backend/helpers/csv_writer.py
+++ backend/helpers/csv_writer.py
@@ -3,6 +3,7 @@ import csv
 from django.http import HttpResponse
 
 from rest_framework.request import Request
+from rest_framework.response import Response
 
 from books.models import Book
 from books.search_filters import generate_filter_from_query_params
@@ -11,9 +12,14 @@ from .csv_export_formatter import CSVExportFormatter
 
 class CSVWriter:
     def __init__(self, csv_export_type: str) -> None:
+        self.csv_export_type = csv_export_type
         self.csv_export_formatter = CSVExportFormatter(csv_export_type)
     
     def write_csv(self, request: Request):
+        default = Response(f"Invalid CSV export type: {self.csv_export_type}")
+        return getattr(self, 'write_csv_' + self.csv_export_type, lambda: default)(request)
+    
+    def write_csv_books(self, request: Request):
         # Turn request.query_params to filter
         filter_kwargs = generate_filter_from_query_params(request.query_params.dict())
         books = Book.objects.filter(**filter_kwargs)

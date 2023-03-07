2d7fadcb582041a2db8f79b08ba2521848669ee1
Casey Szilagyi <75186969+caseyszilagyi@users.noreply.github.com>
2023-03-07 12:23:19 -0500

    Refactor list pages to use common template (#487)

--- frontend/src/pages/genres/GenreList.tsx
+++ frontend/src/pages/genres/GenreList.tsx
@@ -1,13 +1,6 @@
-import { Column } from "primereact/column";
-import {
-  DataTable,
-  DataTablePageEvent,
-  DataTableRowClickEvent,
-  DataTableSortEvent,
-} from "primereact/datatable";
 import { Toast } from "primereact/toast";
 import React, { useRef } from "react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   APIGenreSortFieldMap,
@@ -18,68 +11,52 @@ import {
   GENRES_API,
   GetGenresResp,
 } from "../../apis/genres/GenresAPI";
-import {
-  createColumns,
-  TableColumn,
-} from "../../components/datatable/TableColumns";
+import { TableColumn } from "../../components/datatable/TableColumns";
 import { logger } from "../../util/Logger";
-import { NUM_ROWS } from "../books/BookList";
 import AddPageButton from "../../components/buttons/AddPageButton";
 import LabeledSwitch from "../../components/buttons/LabeledSwitch";
 import SelectSizeButton, {
   SelectSizeButtonOptions,
 } from "../../components/buttons/SelectSizeButton";
 import { Button } from "primereact/button";
-import { isHighlightingText } from "../../util/ClickCheck";
+import ListTemplate from "../../templates/list/ListTemplate";
+import { IDer } from "../../util/IDOps";
 
-// The Genre interface
 export interface Genre {
   id: string;
   name: string;
   bookCount: number;
 }
 
-// Properties of each column that change, the rest are set below when creating the actual Columns to be rendered
-const COLUMNS: TableColumn[] = [
-  {
-    field: "name",
-    header: "Genre",
-    sortable: true,
-    style: { minWidth: "8rem", width: "16rem" },
-  },
-  {
-    field: "bookCount",
-    header: "Number of Books",
-    sortable: true,
-    style: { minWidth: "8rem", width: "12rem" },
-  },
-];
-
 export default function GenreList() {
+  const COLUMNS: TableColumn[] = [
+    {
+      field: "name",
+      header: "Genre",
+      sortable: true,
+      style: { minWidth: "8rem", width: "16rem" },
+    },
+    {
+      field: "bookCount",
+      header: "Number of Books",
+      sortable: true,
+      style: { minWidth: "8rem", width: "12rem" },
+    },
+    {
+      field: "none",
+      header: "To Book List",
+      customBody: (rowData: Genre) => bookFilteredList(rowData),
+      style: { width: "3rem", padding: "2" },
+    },
+  ];
+
   // ----------------- STATE -----------------
-  const [loading, setLoading] = useState<boolean>(false); // Whether we show that the table is loading or not
+  const [isLoading, setIsLoading] = useState<boolean>(false); // Whether we show that the table is loading or not
   const [numberOfGenres, setNumberOfGenres] = useState<number>(0); // The number of elements that match the query
-  const [genres, setGenres] = useState<Genre[]>([]); // The data displayed in the table
-
-  const [rows, setRows] = useState<number>(NUM_ROWS);
+  const [genres, setGenres] = useState<IDer[]>([]); // The data displayed in the table
   const [isNoPagination, setIsNoPagination] = useState<boolean>(false);
-  const [size, setSize] = useState<SelectSizeButtonOptions>(
-    SelectSizeButtonOptions.Small
-  );
-
-  // The current state of sorting.
-  const [sortParams, setSortParams] = useState<DataTableSortEvent>({
-    sortField: "",
-    sortOrder: null,
-    multiSortMeta: null, // Not used
-  });
-
-  // The current state of the paginator
-  const [pageParams, setPageParams] = useState<DataTablePageEvent>({
-    first: 0,
-    rows: rows,
-    page: 0,
-  });
+  const [tableWhitespaceSize, setTableWhitespaceSize] =
+    useState<SelectSizeButtonOptions>(SelectSizeButtonOptions.Small);
 
   // ----------------- METHODS -----------------
   // Navigator used to go to a different page
@@ -91,43 +68,12 @@ export default function GenreList() {
     navigate("/books", { state: { genre: genre.name } });
   };
 
-  // Called when any of the columns are selected to be sorted
-  const onSort = (event: DataTableSortEvent) => {
-    logger.debug("Sort Applied", event);
-    setLoading(true);
-    setSortParams(event);
-  };
-
-  // Called when the paginator page is switched
-  const onPage = (event: DataTablePageEvent) => {
-    logger.debug("Page Applied", event);
-    setRows(event.rows);
-    setLoading(true);
-    setPageParams(event);
-  };
-
-  const onRowClick = (event: DataTableRowClickEvent) => {
-    if (isHighlightingText()) return;
-    const genre = event.data as Genre;
-    logger.debug("Edit Genre Clicked", genre);
-    navigate(`/genres/detail/${genre.id}`);
-  };
-
-  // API call on page load
-  useEffect(() => callAPI(), [sortParams, pageParams, isNoPagination]);
-
   // Calls the Genres API
-  const callAPI = () => {
-    // Invert sort order
-    let sortField = APIGenreSortFieldMap.get(sortParams.sortField) ?? "";
-    if (sortParams.sortOrder == -1) {
-      sortField = "-".concat(sortField);
-    }
-
+  const callAPI = (page: number, pageSize: number, sortField: string) => {
     if (!isNoPagination) {
       GENRES_API.getGenres({
-        page: (pageParams.page ?? 0) + 1,
-        page_size: pageParams.rows,
+        page: page,
+        page_size: pageSize,
         ordering: sortField,
       }).then((response) => onAPIResponse(response));
     } else {
@@ -141,7 +87,7 @@ export default function GenreList() {
   const onAPIResponseNoPagination = (response: APIGenre[]) => {
     setGenres(response.map((genre) => APIToInternalGenreConversion(genre)));
     setNumberOfGenres(response.length);
-    setLoading(false);
+    setIsLoading(false);
   };
 
   // Set state when response to API call is received
@@ -150,7 +96,7 @@ export default function GenreList() {
       response.results.map((genre) => APIToInternalGenreConversion(genre))
     );
     setNumberOfGenres(response.count);
-    setLoading(false);
+    setIsLoading(false);
   };
 
   // ----------------- TEMPLATES/VISIBLE COMPONENTS -----------------
@@ -170,10 +116,7 @@ export default function GenreList() {
     );
   };
 
-  // Toast is used for showing success/error messages
   const toast = useRef<Toast>(null);
-
-  const columns = createColumns(COLUMNS);
 
   const addGenreButton = (
     <div className="flex justify-content-end col-3">
@@ -197,8 +140,27 @@ export default function GenreList() {
 
   const selectSizeButton = (
     <div className="flex col-6 justify-content-center my-1 p-0">
-      <SelectSizeButton value={size} onChange={(e) => setSize(e.value)} />
+      <SelectSizeButton
+        value={tableWhitespaceSize}
+        onChange={(e) => setTableWhitespaceSize(e.value)}
+      />
     </div>
+  );
+
+  const dataTable = (
+    <ListTemplate
+      columns={COLUMNS}
+      detailPageURL={"/genres/detail/"}
+      whitespaceSize={tableWhitespaceSize}
+      isNoPagination={isNoPagination}
+      isLoading={isLoading}
+      setIsLoading={setIsLoading}
+      totalNumberOfEntries={numberOfGenres}
+      setTotalNumberOfEntries={setNumberOfGenres}
+      rows={genres}
+      APISortFieldMap={APIGenreSortFieldMap}
+      callGetAPI={callAPI}
+    />
   );
 
   return (
@@ -211,38 +173,7 @@ export default function GenreList() {
       <div className="flex justify-content-center">
         <div className="card col-8 pt-0 px-3 justify-content-center">
           <Toast ref={toast} />
-          <DataTable
-            showGridlines
-            // General Settings
-            value={genres}
-            lazy
-            responsiveLayout="scroll"
-            loading={loading}
-            size={size ?? "small"}
-            // Operations on rows
-            rowHover
-            selectionMode={"single"}
-            onRowClick={(event) => onRowClick(event)}
-            // Paginator
-            paginator={!isNoPagination}
-            first={pageParams.first}
-            rows={rows}
-            totalRecords={numberOfGenres}
-            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
-            onPage={onPage}
-            rowsPerPageOptions={[5, 10, 15, 25, 50]}
-            paginatorPosition="both"
-            // Sorting
-            onSort={onSort}
-            sortField={sortParams.sortField}
-            sortOrder={sortParams.sortOrder}
-          >
-            {columns}
-            <Column
-              body={bookFilteredList}
-              style={{ width: "3rem", padding: 2 }}
-            />
-          </DataTable>
+          {dataTable}
         </div>
       </div>
     </div>

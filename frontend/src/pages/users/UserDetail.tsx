93221fe6db3b35f55b39387763743d3332d67aed
hac21 <hac21@duke.edu>
2023-03-22 20:44:59 -0400

    done

--- frontend/src/pages/users/UserDetail.tsx
+++ frontend/src/pages/users/UserDetail.tsx
@@ -48,12 +48,7 @@ export default function VendorAdd() {
           setIsModifiable(!checkForSelfEditandAdminEdit(response.username));
           setUserName(response.username);
           setIsAdmin(response.is_staff);
-          setIsPageDeleteable(
-            !(
-              response.username === "admin" ||
-              id === localStorage.getItem("userID")
-            )
-          );
+          setIsPageDeleteable(!checkForSelfEditandAdminEdit);
         })
         .catch(() => showFailure(toast, "Could not fetch user data"));
     }

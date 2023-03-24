146f04a01c8f4f954ed3214195938f4dd0e6cf8f
Casey Szilagyi <75186969+caseyszilagyi@users.noreply.github.com>
2023-03-24 01:54:18 -0400

    Show date and time on bookcase list (#656)

--- frontend/src/util/DateOps.tsx
+++ frontend/src/util/DateOps.tsx
@@ -1,7 +1,13 @@
+import { format } from "date-fns";
+
 export function externalToInternalDate(date: string) {
   return new Date(date.replace("-", "/"));
 }
 
 export function internalToExternalDate(date: Date) {
   return date.toISOString().slice(0, 10);
+}
+
+export function internalToExternalDateTime(date: Date) {
+  return format(date, "MMMM do, yyyy hh:mma");
 }

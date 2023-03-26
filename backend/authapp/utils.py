5335bbf1f9f989ee2f580238479e74567259f317
hkder <samhskim1@gmail.com>
2023-03-26 03:17:54 -0400

    fixed admin permissions

--- backend/authapp/utils.py
+++ backend/authapp/utils.py
@@ -1,12 +1,16 @@
+from utils.general import str2bool
+
 def can_modify(request, instance):
     modifying_user = request.user.username
     modified_user = instance.username
+
     modified_user_is_staff = instance.is_staff
+    modifying_user_is_staff = str2bool(request.data.get('is_staff', 'true'))
 
-    if modified_user == 'admin':
+    if modified_user == 'admin' and modifying_user_is_staff == False:
         return False, "Cannot change admin(SuperUser) account privileges"
-
-    if modifying_user == modified_user:
+    
+    if modified_user_is_staff and modifying_user == modified_user and modified_user_is_staff != modifying_user_is_staff:
         return False, "Cannot change self account privileges"
     
     # if modified_user_is_staff and modifying_user != 'admin':

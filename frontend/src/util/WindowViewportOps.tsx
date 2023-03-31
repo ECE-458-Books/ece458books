e9084e03b4eb9d714eb209ff76e1dc7cb1b7d8b7
hac21 <hac21@duke.edu>
2023-03-31 18:39:44 -0400

    redo

--- frontend/src/util/WindowViewportOps.tsx
+++ frontend/src/util/WindowViewportOps.tsx
@@ -1,4 +1,10 @@
-export function scrollToTop(behavior?: ScrollBehavior): void {
+export enum ViewScrollType {
+  Instant = "instant",
+  Smooth = "smooth",
+  Auto = "auto",
+}
+
+export function scrollToTop(behavior?: ViewScrollType): void {
   window.scrollTo({
     top: 0,
     left: 0,

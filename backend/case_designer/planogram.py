e69d66c03e9bed4d4f23d285cc35684e17caf00c
Casey <caseyszilagyi@gmail.com>
2023-04-01 11:58:19 -0400

    mostly working, some cleanup to be done

--- backend/case_designer/planogram.py
+++ backend/case_designer/planogram.py
@@ -1,12 +1,19 @@
 import io
 from django.http import HttpResponse
-from reportlab.pdfgen import canvas
-from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, Image, KeepTogether, Frame
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, Paragraph, Image, Spacer
 import requests
 
 from .models import Bookcase
 
 class PlanogramGenerator:
+    # Style used for all tables, it creates grid lines and centers the elements
+    table_style = [
+        ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ('ALIGN',(0,0),(-1,-1),'CENTER'),
+        ('VALIGN',(0,0),(-1,-1),'MIDDLE'),
+    ]
+
     def __init__(self, bookcase: Bookcase) -> None:
         self.bookcase = bookcase
         self.shelves = bookcase.shelves.all()
@@ -23,14 +30,14 @@ class PlanogramGenerator:
     def create_document_elements(self):
         elements = []
         elements.append(self.create_display_books_table())
-        elements.append(self.create_layout_table())
+        elements.extend(self.create_shelf_layout_tables())
         return elements
     
     # Display books table, summarizing totals of each book along with some facts about the book and the book image
     def create_display_books_table(self):
-        data = [("Image", "Title", "Author", "ISBN13", "Display Count")]
-        data = self.create_display_books_data()
-        table = Table(data, colWidths=[50, 250, 100, 100, 50])
+        data = [("Image", "Title", "Author", "ISBN13", Paragraph("Display Count"))]
+        data.extend(self.create_display_books_data())
+        table = Table(data, colWidths=[50, 250, 100, 100, 50], style=self.table_style)
         return table  
 
     def create_display_books_data(self):
@@ -38,7 +45,7 @@ class PlanogramGenerator:
         for shelf in self.shelves:
             for display_book in shelf.displayed_books.all():
                 if display_book.book in display_books_dict:
-                    display_books_dict[display_book.book][3] += display_book.display_count
+                    display_books_dict[display_book.book][4] += display_book.display_count
                 else:
                     display_books_dict[display_book.book] = self.create_display_book_row(display_book)
                     
@@ -49,33 +56,29 @@ class PlanogramGenerator:
         for author in display_book.book.authors.all():
             authors += author.name + ", "
         img = self.get_display_book_image(display_book)
-        return [img, Paragraph(display_book.book.title), authors, display_book.book.isbn_13, display_book.display_count]
+        return [img, Paragraph(display_book.book.title), Paragraph(authors), display_book.book.isbn_13, display_book.display_count]
 
     # Layout table, showing information about the books and the order they should be displayed in
     # (similar to the case designer page)
-    def create_layout_table(self):
-        data = self.create_layout_data()
-        table = Table(data, colWidths=[300])
-        return table 
+    def create_shelf_layout_tables(self):
+        tables = []
+        for idx, shelf in enumerate(self.shelves):
+            tables.append(Paragraph(f"Shelf {idx + 1}"))
+            tables.append(Spacer(300, 20))
+            tables.append(self.create_shelf_layout_table(shelf))
+            tables.append(Spacer(300, 20))
+        return tables
     
-    def create_layout_data(self):
-        data = []
-        for shelf in self.shelves:
-            data.append(self.create_layout_shelf(shelf))
-        return data
-    
-    def create_layout_shelf(self, shelf):
-        row = []
+    def create_shelf_layout_table(self, shelf):
+        rows = [("Image", "Title", "Display Count", "Display Mode")]
         for display_book in shelf.displayed_books.all():
-            row.append(self.create_layout_book(display_book))
-        frame = Frame()
-        return row
+            rows.append(self.create_layout_book(display_book))
+        return Table(rows, colWidths=[50, 300, 100, 100], style=self.table_style)
+        
     
     def create_layout_book(self, display_book):
-        
         img = self.get_display_book_image(display_book)
-        val = [img, Paragraph(display_book.book.title)]
-        return
+        return (img, Paragraph(display_book.book.title), display_book.display_count, display_book.display_mode)
         
     def get_display_book_image(self, display_book):
         resp = requests.get(display_book.book.image_url)

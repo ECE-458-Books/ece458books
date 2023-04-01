a3a4a15e100a0ba16fff66c92083a311f5bd0009
Daniel Feinblatt <daniel.feinblatt@duke.edu>
2023-04-01 15:26:43 -0400

    Use external API to identify related books when doing an initial book lookup

--- backend/books/related_books.py
+++ backend/books/related_books.py
@@ -1,5 +1,21 @@
 import re
+from urllib.request import urlopen
+import json, os, io, environ, urllib
+from typing import List
+from lxml import etree
+from isbnlib import *
 
 
 def standardize_title(title: str) -> str:
     return re.sub(r'[^a-zA-Z0-9]', '', title).lower()
+
+
+def get_related_isbns(isbn: str) -> List[str]:
+    related_books_endpoint = f"https://www.librarything.com/api/thingISBN/{isbn}"
+    resp = urlopen(related_books_endpoint)
+    tree = etree.parse(source=resp)
+    return {parse_isbn(xml_isbn.text) for xml_isbn in tree.getroot()}
+
+
+def parse_isbn(isbn: str) -> str:
+    return canonical(to_isbn13(isbn)) if is_isbn10(isbn) else canonical(isbn)
